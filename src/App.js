50149d772240759bf9890ba8b4d2b7bb9e86c404
befflus <befflus@hotmail.no>
2019-01-12 15:47:52 +0100

    App is able to get google map and data from foursquare. (Console logged)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import {load_google_maps, getVenues} from './Utils'
 
 class App extends Component {
 
   
   componentDidMount (){
     // Start with getting data
-    
+    this.getStarted();
   }
+  
+  getStarted(){
+
+    load_google_maps().then(result => console.log(result))
+    getVenues().then(result => console.log(result))
+  }
+  
   render() {
     return (
       <div className="App">
